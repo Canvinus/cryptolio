cc023831cc5c32b7225eb45a7a4f00f0b092c063
Andrey Gruzdev <canvi@Andreys-MacBook-Pro.local>
2023-02-08 21:31:11 +0100

    Token update #2

--- packages/react-app/src/components/Token.js
+++ packages/react-app/src/components/Token.js
@@ -3,8 +3,8 @@ import { Layer, Image, Text } from 'react-konva';
 
 export const Token = (props) => {
   const [image, setImage] = useState(null);
-  const [x, setX] = useState(null);
-  const [y, setY] = useState(null);
+  const [x, setX] = useState(Math.random() * props.stageWidth);
+  const [y, setY] = useState(Math.random() * props.stageHeight);
   const [clicked, setClicked] = useState(false);
 
   const click = () => {
@@ -15,19 +15,45 @@ export const Token = (props) => {
     loadImage();
   }, [props.src]);
 
-  useEffect(() => {
-    setX(Math.random() * props.stageWidth);
-    setY(Math.random() * props.stageHeight);
-  }, []);
-
   const loadImage = () => {
     let img = new window.Image();
     img.src = props.src;
     setImage(img);
   }
 
+  const randomMove = () => {
+    let stepX = 1;
+    if(x <= 0) {
+      stepX = 1;
+    }
+    else if(x >= props.stageWidth) {
+      stepX = -1;
+    }
+    let stepY = 1;
+    if(y <= 0) {
+      stepY = 1;
+    }
+    else if(y >= props.stageHeight) {
+      stepY = -1;
+    }
+    console.log(x, props.stageWidth);
+    console.log(y, props.stageHeight);
+
+    setX(prev => prev + stepX);
+    setY(prev => prev + stepY);
+  }
+
+  useEffect(() => {
+
+    const intervalId = setInterval(() => {
+      randomMove();
+    }, 10);
+    
+    return () => clearInterval(intervalId);
+
+  }, [])
+
   const onDrag = (e) => {
-    console.log(e.target.children)
     const x = e.target.x();
     const y = e.target.y();
 
